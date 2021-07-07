c91d75572de80b0470e782e027789d8b410db22d
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-07-07 18:51:42 -0500

    Login post route functioning.

--- controllers/api-v1/users.js
+++ controllers/api-v1/users.js
@@ -21,7 +21,7 @@ router.post('/register', async (req, res) => {
             email: req.body.email
         })
         // if (user), !register
-        if(findUser) return res.status(400).json({ msg: "User already exists." })
+        if (findUser) return res.status(400).json({ msg: "User already exists." })
 
         // hash password from req.body
         const password = req.body.password
@@ -54,6 +54,41 @@ router.post('/register', async (req, res) => {
 })
 
 // POST /users/login -- validate login credentials
+router.post('/login', async (req, res) => {
+    try {
+        // find user in db
+        const findUser = await db.User.findOne({
+            email: req.body.email
+        })
+
+        const validFail = "Incorrect Username or Password"
+
+        // if (!user) go back
+        if(!findUser) return res.status(400).json({ msg: validFail })
+
+        // check user's password
+        const match = await bcrypt.compare(req.body.password, findUser.password)
+
+        // if !password go back
+        if(!match) return res.status(400).json({ msg: validFail })
+
+        // create jwt payload
+        const payload = {
+            name: findUser.name,
+            email: findUser.email,
+            id: findUser.id
+        }
+
+        // sign jwt, send response
+        const token = await jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: 60 * 60 })
+        res.json({ token })
+
+    } catch(err) {
+        log(err)
+        res.status(500).json({ msg: "Internal Server Error" })
+    }
+})
+
 
 // GET /auth-locked -- redirect if bad jwt is found
 
